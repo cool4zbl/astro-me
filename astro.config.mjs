250a89ae93be3fd5ebd63d87d2dfba32a5776d0e
binliu <binliu.zhang@gmail.com>
2025-10-11 00:00:19 +0200

    fix: update GA script for improved readability and consistency; comment out unused partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-import partytown from '@astrojs/partytown';
-
 export default defineConfig({
     site: 'https://binliuzhang.com/',
     integrations: [mdx(), sitemap(), tailwind({
         applyBaseStyles: true,
     }),
-    partytown({ config: { forward: ['dataLayer.push'] } }),
+    // partytown({ config: { forward: ['dataLayer.push'] } }),
     ],
     markdown: {
         shikiConfig: {
